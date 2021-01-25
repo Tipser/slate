9e1e69cce43fac3625d95fab4be62aff9b0d71bd
Mateusz Sikora <ms1qaz@gmail.com>
2021-01-25 12:44:46 +0100

    [chore] changelog builder (#99)
    * create a simple script to build changelog
    
    * improve styles

--- changelog/scripts/generate_changelog_html.sh
+++ changelog/scripts/generate_changelog_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p build
-cp changelog/resources/github-markdown.css build
-(echo "<html><head><link rel=\"stylesheet\" href=\"github-markdown.css\"></head><body class=\"markdown-body\">"; showdown makehtml -i changelog/tipser-elements/CHANGELOG.md -a; echo "</body><html>") > build/changelog.html
+cp -R changelog/resources/* build
+cat changelog/tipser-elements/CHANGELOG.md | node changelog/scripts/changelog-compiler.js > build/changelog.html

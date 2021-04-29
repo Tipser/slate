367fe92bea58cdb60df08410be1f352e8a1c9346
Mateusz Sikora <ms1qaz@gmail.com>
2021-04-29 14:59:55 +0200

    adjust changelog compiler to work with many changelog files

--- changelog/scripts/generate_changelog_html.sh
+++ changelog/scripts/generate_changelog_html.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p build
 cp -R changelog/resources/* build
-cat changelog/tipser-elements/CHANGELOG.md | node changelog/scripts/changelog-compiler.js > build/changelog.html
+node changelog/scripts/changelog-compiler.js > build/changelog.html
